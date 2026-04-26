28c693dddb8346d23e629270981d9a2ebc3f0d04
Leomar Alves Marcelino <leo.dev.jobs@gmail.com>
2026-04-26 15:25:15 -0300

    feat: implementa as funções para imprimir o estado das pilhas durante a resolução do desafio

--- include/hanoi.h
+++ include/hanoi.h
@@ -6,5 +6,7 @@ using namespace std;
 
 void inicializarHastes(int n, stack<int>& a, stack<int>& b, stack<int>& c);
 void resolverHanoi(int n, stack<int>& origem, stack<int>& destino, stack<int>& auxiliar, char nomeOrigem, char nomeDestino, char nomeAuxiliar, int& contador);
+void imprimirPilha(char nome, stack<int> p);
+void imprimirEstado(stack<int> a, stack<int> b, stack<int> c);
 
 #endif
--- src/hanoi.cpp
+++ src/hanoi.cpp
@@ -9,26 +9,48 @@ void inicializarHastes(int n, stack<int>& a, stack<int>& b, stack<int>& c){
     {
         a.push(i);
     }
-    
 }
 
 void resolverHanoi(int n, stack<int>& origem, stack<int>& destino, stack<int>& auxiliar, char nomeOrigem, char nomeDestino, char nomeAuxiliar, int& contador) {
-    
-    contador = 0;
-
     if (n == 1) {
         int disco = origem.top();
         destino.push(disco);
         origem.pop();
-        cout << "Mover disco " << n << " de " << nomeOrigem << " para " << nomeDestino << endl;
+        cout << "Mover disco " << disco << " de " << nomeOrigem << " para " << nomeDestino << endl;
+        imprimirEstado(origem, auxiliar, destino);
         contador++;
         return;
     }
 
     resolverHanoi(n - 1, origem, auxiliar, destino, nomeOrigem, nomeAuxiliar, nomeDestino, contador);
-
     resolverHanoi(1, origem, destino, auxiliar, nomeOrigem, nomeDestino, nomeAuxiliar, contador);
-
     resolverHanoi(n - 1, auxiliar, destino, origem, nomeAuxiliar, nomeDestino, nomeOrigem, contador);
+}
 
+void imprimirPilha(char nome, stack<int> p){
+    stack<int> temp;
+
+    while(!p.empty()){
+        temp.push(p.top());
+        p.pop();
+    }
+
+    cout << nome << ": [";
+
+    while (!temp.empty()){
+        cout << temp.top();
+        if(temp.size() > 1){
+            cout << ", ";
+        }
+        temp.pop();
+    }
+    
+    cout << "]"; 
+}
+
+void imprimirEstado(stack<int> a, stack<int> b, stack<int> c){
+    imprimirPilha('A', a);
+    imprimirPilha('B', b);
+    imprimirPilha('C', c);
+    cout << endl;
 }
